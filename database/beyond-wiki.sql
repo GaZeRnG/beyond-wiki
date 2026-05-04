6ddd4f91eaefc795d4d8df938e7df4739f4ee75b
GaZeRnG <gazeringg@gmail.com>
2026-05-05 02:08:33 +0800

    .

--- database/beyond-wiki.sql
+++ database/beyond-wiki.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 16, 2026 at 06:07 PM
+-- Generation Time: May 04, 2026 at 07:51 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,18 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `cookie_tokens`
+--
+
+CREATE TABLE `cookie_tokens` (
+  `user_id` int(11) NOT NULL,
+  `token_hash` varchar(64) NOT NULL,
+  `expires_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `login_attempts`
 --
 
@@ -34,9 +46,55 @@ CREATE TABLE `login_attempts` (
   `attempted_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tips`
+--
+
+CREATE TABLE `tips` (
+  `tip_id` int(11) NOT NULL,
+  `tip_title` varchar(255) NOT NULL,
+  `tip_content` varchar(255) NOT NULL,
+  `author` varchar(50) DEFAULT NULL,
+  `created_at` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(50) DEFAULT NULL,
+  `user_email` varchar(100) NOT NULL,
+  `user_password` varchar(255) DEFAULT NULL,
+  `user_pfp` varchar(255) DEFAULT NULL,
+  `oauth_provider` enum('google','discord') DEFAULT NULL,
+  `oauth_id` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_name`, `user_email`, `user_password`, `user_pfp`, `oauth_provider`, `oauth_id`, `created_at`, `updated_at`) VALUES
+(1, 'test', 'gazeringg@gmail.com', '$2y$10$V2RfxkLE7mLf.7mi12HxceZJaVdakvbZhdmmHHD5rcgspjdUaJz5K', NULL, '', NULL, '2026-04-27 14:50:16', '2026-04-27 14:50:16');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `cookie_tokens`
+--
+ALTER TABLE `cookie_tokens`
+  ADD PRIMARY KEY (`token_hash`),
+  ADD KEY `user id` (`user_id`);
 
 --
 -- Indexes for table `login_attempts`
@@ -44,6 +102,19 @@ CREATE TABLE `login_attempts` (
 ALTER TABLE `login_attempts`
   ADD PRIMARY KEY (`id`),
   ADD KEY `ip` (`ip`,`attempted_at`);
+
+--
+-- Indexes for table `tips`
+--
+ALTER TABLE `tips`
+  ADD PRIMARY KEY (`tip_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_email` (`user_email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -54,6 +125,28 @@ ALTER TABLE `login_attempts`
 --
 ALTER TABLE `login_attempts`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tips`
+--
+ALTER TABLE `tips`
+  MODIFY `tip_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cookie_tokens`
+--
+ALTER TABLE `cookie_tokens`
+  ADD CONSTRAINT `user id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
