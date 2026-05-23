344171d5bae7cbc70b4fab4873fdb2bb4c105f10
GaZeRnG <gazeringg@gmail.com>
2026-05-24 01:30:12 +0800

    test3

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     reactCompiler: true,
+
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'cdn.discordapp.com',
+                pathname: '/avatars/**',
+            },
+            {
+                protocol: 'https',
+                hostname: '*.googleusercontent.com',
+            },
+        ],
+    },
 };
 
 module.exports = {
